a9f00fd3469208a217ecd571465aca4cd2d5a8b7
noenadies <33671023+noenadies@users.noreply.github.com>
2018-04-21 20:38:18 -0500

    Update login.js

--- login.js
+++ login.js
@@ -1,11 +1,20 @@
 
+var arraymarkes ;
+var generalkey;
 
-
+var idgeneral=0;
+var objh;
 
   var map;
 var markers = [];
 
+var vnodep="";
+var vnodelat="";
+var vnodelng="";
+var vnodelnguser="";
+var vnodelatuser="";
 
+alert("ok");
 
  // Initialize Firebase
   var config = {
@@ -20,30 +29,209 @@ var markers = [];
 
 
 
+var general;
+
+
+
+unavesmensaje();
 
 
 
 
+var arrnode2=[];
 
 
 
+function unavesmensaje2()
+{
+
+//var c=String(cedu);
+//var cl= String(celu);child("user").
+
+  firebase.database().ref().child("taxis").once('value').then(function(snapshot) {
+    var username = (snapshot.val() && snapshot.val().username) || 'Anonymous';
+    // ...
+    //alert(  snapshot.val());
+    mi=snapshot.val();
+   // alert(cedu+"  ced celu"+celu);
+var s=mi;// firebase.database().ref().child("user");
+generalkey=snapshot.key;
+general=s;
+//console.log("un "+generalkey);
+objh=Object.keys(mi);
+idgeneral=0;
+var colocar;
+for (var indice in objh) {
+   colocar = {idscript: idgeneral, idfirebase:objh[indice]};
+//arrnode2.push(idgeneral,objh[indice]);
+arrnode2.push(colocar);
+//{firstname : "Malcom", lastname: "Reynolds"}
+idgeneral=idgeneral+1;
+//console.log("Objectdfdkeys(s) "+Object.keys(s));
+//console.log("ind'" + indice + "un  " + objh[indice]);
 
 
-var latm=StF(vnodelat);
-var lngm=StF(vnodelng);
-var latus=StF(vnodelatuser);
-var lngus=StF(vnodelnguser);
+}
+/*for (var indi in arrnode2) {
+arrnode2.push(idgeneral,objh[indice]);
+//console.log("Objectdfdkeys(s) "+Object.keys(s));
+//console.log( "fi " + arrnode2[indi]);
+//console.log( "idgeneral " + idgeneral);
 
-function StF(st){
+}*/
 
-return parseFloat(st);
+
+//   console.log( "id " +Object.keys(arrnode2) );
+
+/*
+
+var i;
+for (i = 0; i < arrnode2.length; i++) { 
+console.log( "arrnode2 " + arrnode2[i].idfirebase);
+}
+*/
+
+
+/*
+for (var i=1 ; i<=idgeneral;i=i+1){
+
+//console.log("s "+ Object.keys(arrnode2));
+console.log("s "+ arrnode2[i]);
+}*/
+
+ 
+   // document.getElementById("idcumplimiento").innerHTML = snapshot.val();
+  });
+
+
+}
+
+// tieen los id node de firebase y id  de
+function  arrayidall(){
+var i;
+for (i = 0; i < arrnode2.length; i++) { 
+console.log( "arrnode2 " + arrnode2[i].idfirebase);
+
+
+}
+
+}
+//busco la informacion del json traido 
+// firebase  los valor ejemplo general ceria ya el padres en estecaso
+//de fireabse taxi pues el node  de taxis los hijos  son uno dos y tres  ese seria hijo  nodh
+// un hijo mas profundo nodh2 seria  cedula telefono, nombre ect 
+function  nodepartei(nodh,nodh2){
+//console.log( "general.uno.cedula  = general[nodh][nodh2]  " + general[nodh][nodh2]);
+return general[nodh][nodh2]; 
 }
 
 
-    function initMap() {
-        var uluru = {lat:vnodelat, lng: vnodelng};
-          map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 10,
+var noregistro=true;
+function validadcionuser(cel,ced){
+for(var val in general){
+    console.log("cel "+general[val].cedula);
+   if(general[val].cel==cel&&general[val].cedula==ced){
+ alert("encontrado login ");
+ noregistro=false;
+   } 
+}
+if(noregistro){
+alert("no encontrado login ");
+}
+}
+var idx=0;
+
+
+function myFunction2(){
+ console.log("cel "+general["uno"].cedula);
+//validadcionuser("311","1112");
+//console.log("fffffffff "+general["taxis"]["uno"].lat);
+//showAndroidToast(22);
+}
+
+function llamarandroid(){
+
+    // vreturn=  window.Android.showToast(id,st1,st2);
+ document.getElementById("demo").innerHTML = vreturn;
+}
+
+
+
+function unavesmensaje()
+{
+
+//var c=String(cedu);
+//var cl= String(celu);child("user").
+
+  firebase.database().ref().child("taxis").once('value').then(function(snapshot) {
+    var username = (snapshot.val() && snapshot.val().username) || 'Anonymous';
+    // ...
+    //alert(  snapshot.val());
+    mi=snapshot.val();
+   // alert(cedu+"  ced celu"+celu);
+var s=mi;// firebase.database().ref().child("user");
+generalkey=snapshot.key;
+general=s;
+//console.log("un "+generalkey);
+objh=Object.keys(mi);
+idgeneral=0;
+for (var indice in objh) {
+
+ colocar = {idscript: idgeneral, idfirebase:objh[indice]};
+//arrnode2.push(idgeneral,objh[indice]);
+arrnode2.push(colocar);
+
+//arrnode2.push(idgeneral,objh[indice]);
+//idgeneral=idgeneral+1;
+//console.log("Objectdfdkeys(s) "+Object.keys(s));
+//console.log("ind'" + indice + "un  " + objh[indice]);
+
+
+}
+
+
+
+
+for (var indi in arrnode2) {
+arrnode2.push(idgeneral,objh[indice]);
+//console.log("Objectdfdkeys(s) "+Object.keys(s));
+//console.log( "fi " + arrnode2[indi]);
+//console.log( "idgeneral " + idgeneral);
+
+}/*
+for (var i=1 ; i<=idgeneral;i=i+1){
+
+//console.log("s "+ Object.keys(arrnode2));
+console.log("s "+ arrnode2[i]);
+}*/
+
+ 
+   // document.getElementById("idcumplimiento").innerHTML = snapshot.val();
+  });
+
+
+}
+
+
+
+
+var idx=0;
+
+
+function udatemap(){
+
+
+   initMap1();
+   andridmarker(222, map) ;
+   eventoonclimaker(markerand);
+
+  
+}
+
+    function initMap1() {
+        var uluru = {lat: 4.657883333333333, lng: -74.06214833333334};
+       map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 4,
           center: uluru,
           styles:[
 
@@ -252,16 +440,15 @@ return parseFloat(st);
           ]
         });
 
-
   var image="https://cdn1.iconfinder.com/data/icons/Map-Markers-Icons-Demo-PNG/32/Map-Marker-Marker-Inside-Chartreuse.png";
-
+  
            var marker = new google.maps.Marker({
-           position:{lat:latm, lng:lngm},
+           position:{lat:latm, lng: lngm},
            //  icon: icons[feature.type].icon,
             map: map,
-            title:vndhijo,
-            name:vndhijo,
-         
+            title:"uno",
+            name:"uno",
+            
             icon: image,
 
             activom:"b",
@@ -271,7 +458,7 @@ return parseFloat(st);
 
 
 
-
+   markers.push({idmar:0, lng: marker});
 
    marker.addListener('click', function(e) {
    map.setZoom(13);
@@ -280,21 +467,40 @@ return parseFloat(st);
     console.log("name "+this.name);
      console.log("titulo "+this.idnodefirebase);
     //this.setVisible(false);
-    this.activom="b";
+    this.activom="a";
     //clearMarkers(markesmas);
     console.log(this.activom);//4.635707
 
-//firebase.database().ref().child("taxis").child(String(this.name)).child("activo").set(this.activom);
+    this.activom="a";
+/*
+firebase.database().ref().child("taxis").child(String(this.name)).child("activo").set(this.activom);
    this.activom="a";
-   texto(this.getPosition()+" "+this.title+" a "+this.activom);
-/*firebase.database().ref().child("taxis").child(String(this.name)).child("latuser").set(vlatuser);
+firebase.database().ref().child("taxis").child(String(this.name)).child("latuser").set(vlatuser);
 firebase.database().ref().child("taxis").child(String(this.name)).child("lnguser").set(vlnguser);
  firebase.database().ref().child("taxis").child(String(this.name)).child("msn").set("carrera activa");
  
-*/}
+*/
 
 
 
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+     
+
+      }
+
+     
 
 function toggleBounce() {
   if (marker.getAnimation() !== null) {
@@ -315,17 +521,21 @@ function andridmarker(location, map) {
    map: map,
    icon:imaandroidiconmaker,
    title:"android",
+           
 
   });
+
+ markers.push({idmar:1, lng: markerand});
+
 }
 var vlatuser=0;
 var vlnguser=0;
 function eventoonclimaker(mark){
-    mark.addListener('click', function(e) {
+  mark.addListener('click', function(e) {
     map.setZoom(13);
     map.setCenter(this.getPosition());
     vlnguser=this.getPosition().lng();
-    vlatuser=this.getPosition().lat();
+     vlatuser=this.getPosition().lat();
   
    // alert(this.title);
     console.log("name "+this.title);
@@ -335,13 +545,38 @@ function eventoonclimaker(mark){
     //clearMarkers(markesmas);
     console.log("lat "+this.getPosition());
 
-texto(this.getPosition()+" "+this.title);
+
 
   });
 
 }
 
+setttimeout(true);
 
-function texto(str){
-  document.getElementById("demo").innerHTML = str
+var viniinterval=true;
+function setttimeout(bol){
+
+  viniinterval=bol;
+  if(viniinterval){
+   
+setTimeout(cuenta_atras, 3000);
+
+}
+}
+
+function cuenta_atras(bo){
+
+
+  showAndroidToast("gpsllamar","","","");
+  var boli=bo;
+  if(String(bo)=="undefined"){
+boli=true;}
+
+udatemap();
+ setttimeout(boli);
+
+
+
+//window.location="https://noenadies.github.io/taxistasservicio/";
+
 }
